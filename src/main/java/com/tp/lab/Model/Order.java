d4eea945bfcdb24218ad16da46587d025370c2ed
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2018-10-27 14:34:24 +0200

    Added two exemplary products, Implemented bill printing, finished logical associations.

--- src/main/java/com/tp/lab/Model/Order.java
+++ src/main/java/com/tp/lab/Model/Order.java
@@ -1,4 +1,33 @@
 package com.tp.lab.Model;
 
+import com.tp.lab.Model.Products.Product;
+import java.util.List;
+
 public class Order {
+
+    private List<Product> products;
+
+
+    public Order(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public double getTotalPrice() {
+        return 0;
+    }
+
+    public double getPartialPrice() {
+        return 0;
+    }
+
+
+
+
+
+
+
 }
