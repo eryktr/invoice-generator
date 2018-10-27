783e09f94f2798b950e99c024c1c71d4489fc561
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2018-10-27 11:39:35 +0200

    Project structure set up

--- src/main/java/com/tp/lab/App.java
+++ src/main/java/com/tp/lab/App.java
@@ -1,13 +1,9 @@
 package com.tp.lab;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
     }
 }
---
+++ src/main/java/com/tp/lab/Model/Product.java
@@ -0,0 +1,4 @@
+package com.tp.lab.Model;
+
+public class Product {
+}
